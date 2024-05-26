6ce2c03f4fca0dca1c2c20a0d79f7c7bb1da44e4
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-05-26 11:46:29 +0800

    initial commit with login

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Login</h1>
+  <form @submit.prevent="login">
+    <input type="email" name="email" v-model="email" id="">
+    <input type="password" name="password" v-model="password" id="">
+    <input type="submit" value="Login">
+  </form>
+</template>
+
+<script>
+import axios from "@/lib/axios";
+export default{
+  data(){
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    async login(){
+      try {
+        const response = await axios.post('/login', {
+          email: this.email,
+          password: this.password
+        });
+
+        console.log(response.data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+</script>
