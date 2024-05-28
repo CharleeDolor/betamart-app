24257ddc7f842abe82a82052f792c1b81024e012
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-05-28 19:07:05 +0800

    navbar added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,33 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <button @click="logout">Logout</button>
-            </li>
-        </ul>
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-brand btn-link">
+            <img :src="require('@/assets/logo-ecommerce.png')" width="30" height="30" alt="">
+            LAZAPEE
+        </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <button @click="logout">Logout</button>
+        </div>
     </nav>
 </template>
 <script>
 import axios from '@/lib/axios';
 export default {
     computed: {
-        getAccountDetails(){
+        getAccountDetails() {
             return this.$store.getters.getAccountDetails;
         }
     },
@@ -36,3 +51,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.btn-link {
+    border-color: transparent;
+}
+</style>
